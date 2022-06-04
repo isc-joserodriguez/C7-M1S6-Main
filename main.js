2d497f4086df54c42f1282763840dc74cb0401f7
Jose Rodriguez <isc.joserodriguez@gmail.com>
2022-06-04 09:23:08 -0600

    Arreglos I

---
+++ main.js
@@ -0,0 +1,16 @@
+/* var arreglo = [
+    [1, 2, 3],
+    [1, 2, 3], 
+    [1, 2, 3]
+] */
+
+
+var arreglo = [1, 2, 3, 'cadena', true, [], [1, 2, 3], [true, false], [12, 'hola', ['Hey', true]]]
+
+console.log(arreglo)
+
+console.log(arreglo[3]);
+console.log(arreglo[4]);
+console.log(arreglo[8][1]);
+console.log(arreglo[8][2]);
+console.log(arreglo[8][2][0]);
